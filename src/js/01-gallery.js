bb880043567f43074a54b2e152d1494290163108
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2022-09-28 20:55:03 +0300

    added markup and code for SimpleLightmbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,31 @@
 // Add imports above this line
 import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import { galleryItems } from './gallery-items';
 
 // Change code below this line
 
 console.log(galleryItems);
 
-console.log(SimpleLightbox);
+//onsole.log(SimpleLightbox);
+
+const markup = galleryItems.reduce((acc, {original, preview, description} = item) => 
+    acc + `<a class="gallery__item" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+    </a>`, "");
+const imagesContainer = document.querySelector('.gallery');
+imagesContainer.insertAdjacentHTML('beforeend', markup);
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+    /* options */
+    captionsData: "alt",
+    captionDelay: 250 
+});
 
