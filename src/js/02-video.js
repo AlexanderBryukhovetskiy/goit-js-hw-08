f8caedd4c518b7e2a455900af993dbbedd3be399
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2022-10-01 20:21:55 +0300

    2 task -
    bug: запис у сховище виконується не при постановці відео на паузу, а при запуску перегляду. Тобто, якщо включити відео спочатку (у сховищі не було запису), з'явться запис, що переглянуто 0 часу; якщо далі подивитись певний час і вперше поставити перегляд відео на паузу, то у сховищі запис не оновиться. При повторному  запуску відео запис у сховищі вже обновиться.
    Отже баг заключається у наступному: якщо перезавантажити сторінку після  першої паузи, відео почне програватися спочатку. Якщо ж перезавантажити сторінку після 2-ї та будь-якої наступної паюзи, плеєр програвати буде не з останньої, а з передостанньої паузи (бо у сховище відбувається саме час відеоряду, на якому  запускається відео ).

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import VimeoPlayer from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+
+let currentTime;
+
+if (!JSON.parse(localStorage.getItem("videoplayer-current-time"))){
+    currentTime = 0;
+} else {
+    currentTime = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+}
+
+console.log("currentTime after player.on()", currentTime);
+
+player.setCurrentTime(currentTime);
+
+player.on('play', function(timeupdate) {
+
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(timeupdate));
+
+    currentTime = JSON.parse(localStorage.getItem("videoplayer-current-time")); 
+    
+    console.log("currentTime inside player.on():", currentTime);
+    console.log('played the video!');
+});
