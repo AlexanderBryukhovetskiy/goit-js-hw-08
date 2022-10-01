ff350edd4c471f1b6da4583be72251f9a573fba3
AlexanderBryukhovetskiy <a.v.bryukhovetskiy@gmail.com>
2022-10-01 22:50:13 +0300

    task-2 : error when start 1-st time

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,13 +3,21 @@ import VimeoPlayer from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new VimeoPlayer(iframe);
 
-let currentTime = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+let currentTime;
 
-if (!currentTime){
+// if (!JSON.parse(localStorage.getItem("videoplayer-current-time"))){
+//     currentTime = 0;
+// } else {
+//     currentTime = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+// }
+
+if (!localStorage.getItem("videoplayer-current-time")){
     currentTime = 0;
-} 
-player.setCurrentTime(currentTime.seconds);
+} else {
+    currentTime = JSON.parse(localStorage.getItem("videoplayer-current-time"));
+}
 
+player.setCurrentTime(currentTime.seconds);
 
 player.on('play', function(timeupdate) {
 
